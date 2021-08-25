2af2d29cecd663eb743eb357c8ebe92d1b4eed5f
Rafael Lima <rafaelbernardes0@gmail.com>
2021-08-25 20:17:31 -0300

    Adicionando Estado e Cidade

---
+++ src/main/java/com/udemy/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.udemy.cursomc.repositories;
+
+import com.udemy.cursomc.domain.Cidade;
+import com.udemy.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
