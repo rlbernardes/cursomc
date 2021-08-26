554885ccc0f8a821462e6dbd28acd7edf8ad7a5d
Rafael Lima <rafaelbernardes0@gmail.com>
2021-08-26 20:13:17 -0300

    Endpoint /clientes{id} disponivel

---
+++ src/main/java/com/udemy/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.udemy.cursomc.services;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+        return optionalCliente.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Cliente.class.getName() ));
+    }
+
+}
