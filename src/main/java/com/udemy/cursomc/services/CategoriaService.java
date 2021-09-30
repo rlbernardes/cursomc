01f8877bf27736e3a3b1e50789559bad584db4e5
Rafael Lima <rafaelbernardes0@gmail.com>
2021-09-30 15:37:30 -0300

    Inserindo nova categoria com POST

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -19,4 +19,8 @@ public class CategoriaService {
         return optionalCategoria.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName() ));
     }
 
+    public Categoria insert(Categoria categoria) {
+        categoria.setId(null);
+        return categoriaRepository.save(categoria);
+    }
 }
