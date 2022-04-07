d1d8221a38cfd8d0d9395b2abd839b91dd6d6892
Rafael Lima <rafaelbernardes0@gmail.com>
2022-04-07 10:02:01 -0300

    Inserindo Pedido

--- src/main/java/com/udemy/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComCartao.java
@@ -1,19 +1,21 @@
 package com.udemy.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
 import javax.persistence.Entity;
 
 @Entity
-public class PagamentoComCartão extends  Pagamento{
+@JsonTypeName("pagamentoComCartao")
+public class PagamentoComCartao extends  Pagamento{
     private static final long serialVersionUID = 1L;
 
     private Integer numeroDeParcelas;
 
-    public PagamentoComCartão() {
+    public PagamentoComCartao() {
     }
 
-    public PagamentoComCartão(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+    public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
         super(id, estado, pedido);
         this.numeroDeParcelas = numeroDeParcelas;
     }
--- src/main/java/com/udemy/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/cursomc/services/PedidoService.java
@@ -1,22 +1,61 @@
 package com.udemy.cursomc.services;
 
+import com.udemy.cursomc.domain.ItemPedido;
+import com.udemy.cursomc.domain.PagamentoComBoleto;
 import com.udemy.cursomc.domain.Pedido;
+import com.udemy.cursomc.domain.enums.EstadoPagamento;
+import com.udemy.cursomc.repositories.ItemPedidoRepository;
+import com.udemy.cursomc.repositories.PagamentoRepository;
 import com.udemy.cursomc.repositories.PedidoRepository;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class PedidoService {
+    PedidoRepository pedidoRepository;
+    PagamentoRepository pagamentoRepository;
+    BoletoService boletoService;
+    ProdutoService produtoService;
+    ItemPedidoRepository itemPedidoRepository;
 
     @Autowired
-    PedidoRepository pedidoRepository;
+    public PedidoService(PedidoRepository pedidoRepository, BoletoService boletoService, PagamentoRepository pagamentoRepository, ProdutoService produtoService, ItemPedidoRepository itemPedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+        this.boletoService = boletoService;
+        this.pagamentoRepository = pagamentoRepository;
+        this.produtoService = produtoService;
+        this.itemPedidoRepository = itemPedidoRepository;
+    }
 
     public Pedido find(Integer id){
         Optional<Pedido> optionalPedido = pedidoRepository.findById(id);
         return optionalPedido.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: " + Pedido.class.getName() ));
     }
 
+    @Transactional
+    public Pedido insert(Pedido pedido){
+        pedido.setId(null);
+        pedido.setInstate(new Date());
+        pedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+        pedido.getPagamento().setPedido(pedido);
+        if(pedido.getPagamento() instanceof PagamentoComBoleto){
+            PagamentoComBoleto pagto = (PagamentoComBoleto) pedido.getPagamento();
+            boletoService.preenchePagamentoComBoleto(pagto, pedido.getInstate());
+        }
+        pedido = pedidoRepository.save(pedido);
+        pagamentoRepository.save(pedido.getPagamento());
+
+        for(ItemPedido item: pedido.getItens()){
+            item.setDesconto(0.0);
+            item.setPreco(produtoService.find(item.getProduto().getId()).getPreco());
+            item.setPedido(pedido);
+        }
+        itemPedidoRepository.saveAll(pedido.getItens());
+        return pedido;
+    }
 }
